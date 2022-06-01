2ce48e35f8a1dd3e6a3dbcc94639989bdb387a03
ismet kizgin <info@ismetkizgin.com.tr>
2022-06-01 23:56:05 +0300

    feat: create telegram bot example

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello Word')
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome", {
+    reply_markup: {
+      keyboard: [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]],
+    },
+  });
+});
+
+bot.onText(/\/sendpic/, (msg) => {
+  bot.sendMessage(msg.chat.id, "ismet kizgin");
+});
